e7ae5d892bf32598565a6d59e5df808ade097b85
Leisan <fleisan18@mail.ru>
2022-09-12 08:14:26 +0500

    added all works from the course

---
+++ Занятие 7 Подключение к базам данных/Материалы для домашки/postgresql_secret.py
@@ -0,0 +1,7 @@
+pg_secret = {
+    "db_host": "178.154.254.133",
+    "db_name": "skyeng",
+    "db_password": "readonlyuser",
+    "db_port": "5432",
+    "db_user": "readonlyuser"
+}
---
+++ Занятие 7 Подключение к базам данных/Материалы для занятия/clickhouse_secret.py
@@ -0,0 +1,7 @@
+ch_secret = {
+    "db_host": "178.154.254.133",
+    "db_name": "skyeng",
+    "db_password": "admin",
+    "db_port": "9000",
+    "db_user": "admin"
+}
